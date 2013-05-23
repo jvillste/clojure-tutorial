973c65bfc3f70fe7c8ac457b76bf17ecb2d16703
Jukka Villstedt <jukka.villstedt@gmail.com>
2013-05-23 14:55:34 +0300

    x

---
+++ project.clj
@@ -0,0 +1,3 @@
+(defproject clojure-tutorial "1.0.0-SNAPSHOT"
+  :description "A clojure tutorial"
+  :dependencies [[org.clojure/clojure "1.4.0"]])
---
+++ src/clojure_tutorial.clj
@@ -0,0 +1,66 @@
+;; namespace
+
+(ns clojure-tutorial)
+
+
+;; numbers
+
+1
+1.2
+1/3
+84762146234912363219874326149871234692138746
+
+
+;; srings
+
+"foo"
+
+"foo
+bar"
+
+
+
+;; keywords
+
+:foo
+
+:bar
+
+:foo.bar/foobar
+
+
+
+;; linked lists
+
+'(1 2 3)
+
+'(1 :foo "bar")
+
+
+
+;; vectors
+
+[1 2 3]
+
+[1 :foo "bar"]
+
+
+
+;; hash maps
+
+{:a 1
+ :b 2}
+
+{:foo {:bar "foobar"}}
+
+
+;; sets
+
+#{:foo :bar}
+#{1 2 3 :foo "bar"}
+
+
+
+
+(map inc [1 2 3])
+
